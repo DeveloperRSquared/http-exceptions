e6b9f54f1f9e92325b9c698402ee7e63a932df53
Rikhil Raithatha <23627977+rikhilrai@users.noreply.github.com>
2021-10-08 18:18:21 +0100

    feature: remove fastapi + rename details -> message (#12)

--- src/http_exceptions/http_exception.py
+++ src/http_exceptions/http_exception.py
@@ -2,23 +2,29 @@
 from __future__ import annotations
 
 from typing import Mapping
+from typing import Optional
+from typing import Type
 from typing import Union
 
-from fastapi import HTTPException as FastAPIHTTPException
 
+class HTTPException(Exception):
+    def __init__(self, status_code: Optional[int] = None, message: Optional[str] = None) -> None:
+        super().__init__()
+        self.status_code = status_code or 500
+        self.message = message
 
-class HTTPException(FastAPIHTTPException):
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(status_code={self.status_code!r}, message={self.message!r})'
+
     def __str__(self) -> str:
         return self.__repr__()
 
-    @staticmethod
-    def from_status_code(status_code: int) -> HTTPException:
+    @classmethod
+    def from_status_code(cls, status_code: int) -> Type[HTTPException]:
         from .client_exceptions import CLIENT_EXCEPTIONS
         from .client_exceptions import TYPE_CLIENT_EXCEPTIONS
         from .server_exceptions import SERVER_EXCEPTIONS
         from .server_exceptions import TYPE_SERVER_EXCEPTIONS
 
         http_exceptions: Mapping[int, Union[TYPE_SERVER_EXCEPTIONS, TYPE_CLIENT_EXCEPTIONS]] = {**SERVER_EXCEPTIONS, **CLIENT_EXCEPTIONS}
-        if status_code not in http_exceptions:
-            raise ValueError(f"{status_code} is not a valid HTTP error code.")
-        return http_exceptions[status_code]()
+        return http_exceptions.get(status_code, cls)
