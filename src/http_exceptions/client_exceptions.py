1689db20eb1c210b0354b367d78304e0f17be7c5
Rikhil <23627977+rikhilrai@users.noreply.github.com>
2021-10-09 18:25:53 +0100

    .

--- src/http_exceptions/client_exceptions.py
+++ src/http_exceptions/client_exceptions.py
@@ -123,7 +123,7 @@ class ExpectationFailedException(ClientException):
 
 class ImATeapotException(ClientException):
     def __init__(self, message: Optional[str] = None) -> None:
-        message = message if message else "I'm a teapot"
+        message = message if message else 'This server is a teapot, not a coffee machine'
         super().__init__(status_code=418, message=message)
 
 
